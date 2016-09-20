3dec9413bcaef33797cc174535677f3c50cb8b9c
cickib <bellakocsis06@gmail.com>
2016-09-20 09:20:44 +0200

    Step 2

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 
 class Contact:
+
+    all_contacts = []
+
     def __init__(self, name, email):
         self.name = name
         self.email = email
+        self.add_contacts()
 
-    
+    def add_contacts(self):
+        self.all_contacts.append(self)
+
+
